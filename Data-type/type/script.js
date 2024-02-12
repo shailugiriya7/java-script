ff11b0f2dbd8d9c68f92d735994ff69a7399fa6a
shailugiriya7 <sgiriya84@gmail.com>
2024-02-12 23:38:37 +0530

    datatype summery

---
+++ Data-type/type/script.js
@@ -0,0 +1,23 @@
+ 
+ 
+ //primitive type
+
+ // 7 types : String ,Number,Boolean, null[empty],undefiend ,Symbol, Bigint 
+
+ //Reference types // (non primitive) types  =>memory allocation allotted for
+
+ // Array ,Objects, Functions
+
+//bigint
+
+// const bigNumber=1235556555665n
+
+const score =231;
+console.log(typeof score )  // number
+const scoreOne=null
+console.log(typeof scoreOne ) // object
+const userid=false //boolean
+const username="shailu"; // type string
+let userOne;  // undefiend
+
+//function  data type= is object function
