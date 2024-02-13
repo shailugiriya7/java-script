b7f6ecad7ac50b0cde889423da4d7fd57f1dea81
shailugiriya7 <sgiriya84@gmail.com>
2024-02-13 16:23:21 +0530

    "parameters[arg]

---
+++ function/fun_01.js
@@ -0,0 +1,35 @@
+function add(num1 ,num2){
+   return num1+num2;
+}
+console.log(add(12,12));
+
+
+const Add=function(num1,num2){
+   console.log(num1+num2);
+}
+Add(10,20)
+
+function squer(num){
+   return num*num;
+}
+const result=(squer(58));   // => variable
+console.log(result) 
+
+
+function loginUser(username){
+   if (!username)  {  // username=== undefined
+       console.log("Please enter a username")
+       return
+   }
+   return `${username} loged in`;
+}
+console.log(loginUser())  // undefined loginUser
+// console.log(loginUser("jhon"))
+
+
+ // default parameters for login
+function User(username="sam"){
+   return `${username} loged in`
+}
+// console.log(User())
+console.log(User("vishal")) // => argument "vishal"
